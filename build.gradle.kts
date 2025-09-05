3156384cfa2b8a1ec2583bf0377d8b28f8ea7277
pipa69 <etskozakk@gmail.com>
2025-09-05 23:18:22 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application") version "8.0.0" apply false
     id("com.android.library") version "8.0.0" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    // DODAJ TO dla Hilt:
+    id("com.google.dagger.hilt.android") version "2.48.1" apply false
 }
 
 tasks.register("clean", Delete::class) {
